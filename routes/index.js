e09828f70141bf79a0b401a326c3a792b2f67468
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 13:10:08 -0600

    i

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,20 @@ var router = express.Router();
 var db = require("../database/index");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  // console.log(db);
-  res.render("index", { title: "Express" });
+router.get(`/:id`, function(req, res, next) {
+  let id = req.params.id;
+  db.getProduct(id, (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(results);
+    }
+  });
 });
+
+// router.get("/", function(req, res, next) {
+//   console.log("lll");
+//   res.render("index", { title: "Express" });
+// });
 
 module.exports = router;
