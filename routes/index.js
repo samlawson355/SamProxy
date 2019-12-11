064c25579c7d70917c659962bfc745143169ddc6
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 10:34:58 -0600

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ let router = express.Router();
 /* GET home page. */
 router.get("/", (req, res, next) => {
   // res.render("index", { title: "Express" });
-  res.send(req);
+  res.send("oops");
   res.end();
 });
 
