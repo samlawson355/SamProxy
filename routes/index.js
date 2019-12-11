8daf235f58321e9f6e701881f40c338f60eff536
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 10:51:33 -0600

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,21 @@ let router = express.Router();
 let db = require("../database/index.js");
 
 /* GET home page. */
-// router.get("/", (req, res, next) => {
-
-//   // res.render("index", { title: "Express" });
-//   res.send("oops");
-//   res.end();
-// });
-
-router.get(`/:id`, (req, res) => {
-  let id = req.params.id;
-  db.getProduct(id, (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(results);
-    }
-  });
+router.get("/", (req, res, next) => {
+  // res.render("index", { title: "Express" });
+  res.send("oops");
+  res.end();
 });
+
+// router.get(`/:id`, (req, res) => {
+//   let id = req.params.id;
+//   db.getProduct(id, (err, results) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(results);
+//     }
+//   });
+// });
 
 module.exports = router;
