d44f4c83145996b5c022a39b620f850bfb485203
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 11:01:50 -0600

    idk

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,9 @@
-let express = require("express");
-let router = express.Router();
-let db = require("../database/index.js");
+var express = require("express");
+var router = express.Router();
 
 /* GET home page. */
-router.get(
-  "http://node-express-env.bcgwijp6ik.us-east-2.elasticbeanstalk.com/",
-  (req, res, next) => {
-    // res.render("index", { title: "Express" });
-    console.log("get");
-    res.send("oops");
-    res.end();
-  }
-);
-
-// router.get(`/:id`, (req, res) => {
-//   let id = req.params.id;
-//   db.getProduct(id, (err, results) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.send(results);
-//     }
-//   });
-// });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
 module.exports = router;
