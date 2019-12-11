b967cde0fcd71b94c851b4fa3afa9f7fe12caf7c
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 08:58:22 -0600

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.send("hi Sam");
-  res.end();
-  // res.render('index', { title: 'Express' });
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
