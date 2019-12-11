940a8a4d322d2f17dc14460f4944c4d548e74b9a
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 11:12:34 -0600

    idk

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
-var db = require("../database/index.js");
+var db = require("../database/index");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
+  console.log(db);
   res.render("index", { title: "Express" });
 });
 
