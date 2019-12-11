e1a1c6c8fb834085945f760008db17694b3ae417
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 11:20:49 -0600

    i

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-// var db = require("../database/index");
+var db = require("../database/index");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
