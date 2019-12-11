8ec9950b98269e9574561eda95850c7959df378b
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 11:15:27 -0600

    i

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  console.log(db);
+  // console.log(db);
   res.render("index", { title: "Express" });
 });
 
