7f5f6b0cbc5ee8a5fbf6d6637738bfc54bb86cab
Sam Lawson <samlawson@Sams-MacBook-Pro.local>
2019-12-11 15:03:45 -0600

    i

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var db = require("./database/index");
 const indexRouter = require("./routes/index");
 
 const app = express();
-
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -18,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 app.use("/", indexRouter);
 
